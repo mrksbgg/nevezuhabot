bd6c8c4d88ee77686c160c386bbdf168a8718de5
Marat Sibagatullin <admin@mrksbgg.ml>
2024-01-10 00:52:55 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import sqlite3
+import os
+
+from pyrogram import Client, idle
+from pyrogram.handlers import MessageHandler
+from config import (
+    token as bot_token,
+    api_id,
+    api_hash,
+    version,
+    adminid
+)
+
+app = Client(
+    "nevezuhabot",
+    api_id=api_id,
+    api_hash=api_hash,
+    bot_token=bot_token
+)
+
+# этот бот был сделан за минут 5, и то на основе из другого моего бота
+@app.on_message()
+def nevezuha(client, message):
+    app.send_video(message.chat.id, "nevezuha.mp4")
+
+
+if __name__ == '__main__':
+    my_handler = MessageHandler(nevezuha)
+    app.add_handler(my_handler)
+    try:
+        app.start()
+        me = app.get_me()
+    except sqlite3.OperationalError as e:
+        print(f"[Error] Ошибка: Сессия занята другим процессом! ({e})")
+        os.remove("nevezuhabot.session")
+        app.start()
+        me = app.get_me()
+    print(f"[Info] Бот был запущен и авторизован как {me.first_name} (@{me.username})")
+    app.send_message(adminid, f"Бот запущен! Версия: v{version}")
+    idle()
